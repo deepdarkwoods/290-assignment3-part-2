2002eeb3271af0feaad0711a47bf198934a93969
howdy dudy <john@nothing.com>
2015-04-26 16:06:32 -0500

    Redo Project

--- demo.js
+++ demo.js
@@ -1,233 +1,257 @@
 
+//initialize section
+var pagesToDisplay = 1;
+var languages = [];
+var httpRequest = [];
+var favList = [];
 
-var originalGistList = [];
-var favoritesList = [];
-var httprequest;
 
 
-var fetchData = function()
+document.getElementById("pages2display").onchange = function()
+    {
+    
+        var temp = document.getElementById("pages2display");
+        pagesToDisplay = temp.options[temp.selectedIndex].value;
+        localStorage.setItem("pages",pagesToDisplay);
+        
+    }
+    
+
+
+document.getElementById("select-lang").onchange = function()
+    {
+        var list = document.getElementById("select-lang");        
+        setLanguage(list);
+    }
+
+function setLanguage (langArray)
+    {
+        //clear current values
+        languages = [];
+        
+        for(var i=0;i<langArray.length;i++)
+        {
+         if (langArray[i].selected)
+             {
+             languages.push(langArray[i].value);
+             console.log(langArray[i].value);
+             }
+        }    
+        
+          localStorage.setItem("Language",languages);
+    }
+    
+    
+function searchGist ()
+    
+    {
+        
+        httpRequest = new XMLHttpRequest();
+        httpRequest.onreadystatechange = waiting;        
+        httpRequest.open('GET', 'https://api.github.com/gists/public', true);
+        httpRequest.send(null);              
+        
+        
+    }
+    
+    
+function waiting()
+
 {
-
-	httprequest = new XMLHttpRequest();
-	if(!httprequest)
-		{
-			throw 'Unable to create httprequest';
-		}
-
-
-		var numpages = document.getElementById('pagesToDisplay').value;
-		
-
-		console.log(numpages);
-
-		if(numpages ==1)
-		{
-			var url = 'https://api.github.com/gists/public?page=1&per_page=100';
-		}
-
-		else if(numpages ==2)
-		{
-			var url = 'https://api.github.com/gists/public?page=2&per_page=100';
-		}
-		
-		else if(numpages ==3)
-		{
-			var url = 'https://api.github.com/gists/public?page=3&per_page=100';
-		}
-		else if(numpages ==4)
-		{
-			var url = 'https://api.github.com/gists/public?page=4&per_page=100';
-		}
-		else if(numpages ==5)
-		{
-			var url = 'https://api.github.com/gists/public?page=5&per_page=100';
-		}
-
-
-
-
-
-		httprequest.open('GET',url,true);
-		httprequest.send();
-		httprequest.onreadystatechange = alertContents;
-	
-};
-
-
-function alertContents()
-{
-	
-	if (this.readyState === 4 && this.status===200)
-		{
-			
-
-
-			originalGistList = httprequest.responseText;
-			localStorage.setItem("rawGistData",(originalGistList));
-			generateGistHtml();
-		
-		}
-
+    if(httpRequest.readyState == 4 && httpRequest.status == 200)
+    {
+    console.log("inside httprequest");
+    localStorage.setItem("gistData",httpRequest.responseText);
+    var parsedData = JSON.parse(httpRequest.responseText);
+    
+    displayResults(parsedData);
+    }
+    
 }
 
 
-function generateGistHtml ()
+function displayResults (gistArray)
 
-{
-		var parsedGistList = JSON.parse(originalGistList);
-		var topElement =document.getElementById("tablestart");
-		var tbl = document.createElement("table");
-		var tblBody = document.createElement("tbody");
+    {
+           
+        
+        var tableTop = document.getElementById("gistd");
+        var tbl     = document.createElement("table");
+        var tblBody = document.createElement("tbody");
+        
+        
+        for(var i =0;i<gistArray.length;i++)
+        {
+            var row = document.createElement("tr");
+            
+            //id
+            var cell1 = document.createElement("td");
+            var cell1t = document.createTextNode(gistArray[i].id);
+           
+          
+            //url
+            var cell2 = document.createElement("a");
+            var cell2t = document.createTextNode(gistArray[i].url);
+            var myLink = gistArray[i].url;
+            cell2.href =myLink;
+            
+            //description
+            var cell3 = document.createElement("td");
+            var cell3t = document.createTextNode(gistArray[i].description);
+            
+            //favorite buttton
+            var cell4 = document.createElement("button");          
+            cell4.type="button";
+            cell4.innerHTML = " + Fav";
+            cell4.setAttribute("gistId", gistArray[i].id);
+            cell4.onclick = function (){
+                var gistId = this.getAttribute("gistId");
+                addToFavList (gistId);
+            }
+          
+          
+         
+            
+            
+            //add text to cells
+            cell1.appendChild(cell1t);
+            cell2.appendChild(cell2t);
+            cell3.appendChild(cell3t);
+          
+            
+            
+            
+            
+            //add cells to row
+            row.appendChild(cell1);
+            row.appendChild(cell2);
+            row.appendChild(cell3);
+            row.appendChild(cell4);
+            tblBody.appendChild(row);
+            
+        }
+        
+        //add rows to main table
+       
+        tbl.appendChild(tblBody);
+        tableTop.appendChild(tbl);     
+        
+        
+        
+    }
+    
+    
 
-
-	for(var x=0; x<parsedGistList.length; x++)
-		{
-		var row = document.createElement("tr");
-
-		var rowLink = document.createElement("a");
-		rowLink.href = parsedGistList[x].url;
-
-		
-		var cell = document.createElement("td");
-		var cell2 = document.createElement("td");
-		var cell3 = document.createElement("td");
-		var link = document.createElement("td");
-		
-
-		var cellText = document.createTextNode(parsedGistList[x].url);
-		var cellTextDescription = document.createTextNode(parsedGistList[x].description);
-		//favoritebutton
-		var fbutton = document.createElement("button");
-		fbutton.innerHTML = "Add to Favorite + ";
-		fbutton.setAttribute("gistID",parsedGistList[x].id);
-
-		fbutton.onClick = function() {
-			var gistId = this.getAttribute("gistId");
-			favoritesList.push(gistId);
-			localStorage.setItem("favorite",(favoritesList));
-		};
-
-		
-
-
-		link.appendChild(rowLink);
-		cell.appendChild(cellTextDescription);
-		cell2.appendChild(cellText);
-		cell3.appendChild(fbutton);
-		row.appendChild(link);
-		row.appendChild(cell);
-		row.appendChild(cell2);
-		row.appendChild(cell3);
-		tblBody.appendChild(row);
-		}
-	
-		
-		tbl.appendChild(tblBody);
-		topElement.appendChild(tbl);
-
-		generateFavoriteList();
-}
-
-
-var addToFavorites = function(gistID)
-{
-console.log("adding to fav");
-console.log(gistID);
-
-
-
-};
-
-function generateFavoriteList()
-{
-
-var list = localStorage.getItem("rawGistData");
-var plist = JSON.parse(list);
-
-var topElement =document.getElementById("favstart");
-var tbl = document.createElement("table");
-var tblBody = document.createElement("tbody");
-
-for(var x=0; x<plist.length; x++)
-{
-	
-
-}
-
-}
-
-
-/*
-
-document.getElementById("githubreq").onclick = function(){searchGitHub();};
-
-
-function searchGitHub()
-{
-	var httprequest = new XMLHttpRequest();
-	if(!httprequest)
-
-		{
-			throw 'Unable to create httprequest';
-		}
-
-		var temp = document.getElementsByName("pages")[0];
-		var pagesRequested = temp.toString();
-
-		var url = 'https://api.github.com/gists'; + '?page=' + temp + '&per_page=30';
-
-		httprequest.onreadystatechange = alertContents;
-		httprequest.open('GET',url,true);
-		httprequest.send();
-
-	}
-
-
-function alertContents()
-{
-	
-	if (this.readyState === 4 && this.status===200)
-	{
-		
-		localStorage.setItem("rawGistData",(this.responseText));
-	}
-
-}
-
-
-function listResults()
-{
-	
-	var results = localStorage.getItem("rawGistData");
-	var presults = JSON.parse(results);
-
-	var table= document.createElement("table");
-	var tableBody = document.createElement("tbody");
-
-
-	for(var i = 0;i < presults.length; i++)
-	{
-		var row = document.createElement("tr");
-
-		//url
-		var cell1 = document.createElement("td");
-		var cellText1 = document.createTextNode(presults[i].url);
-		cell1.appendChild(cellText1);
-		//document.write(i + ":"+ presults[i].url + "<br>");
-		tableBody.appendChild(row);
-	}
-		table.appendChild(tableBody);
-		var mainstruct = document.getElementById("tablestart");
-		mainstruct.appendChild(table);
-
-
-}
-
-listResults();
-
-
-*/
-
-
-
-
+    function addToFavList(gistID)
+    {
+    var originalList = JSON.parse(localStorage.getItem("gistData"));
+        for(x=0;x<originalList.length;x++)
+        {
+            if (originalList[x].id == gistID)
+            {
+                var newFav = JSON.stringify(originalList[x]);
+                localStorage.setItem("FavList",newFav);
+                originalList.splice(x,1);
+               
+                localStorage.setItem("gistData",JSON.stringify(originalList));            
+                
+            }               
+            
+        }      
+        
+ 
+        displayFavorites();
+        
+    }
+    
+    
+   function displayFavorites()
+    {
+        var favoriteList = JSON.parse(localStorage.getItem("FavList"));
+         
+        var tableTop = document.getElementById("favd");
+        var tbl     = document.createElement("table");
+        var tblBody = document.createElement("tbody");
+        
+        
+        for(var i =0;i<favoriteList[0];i++)
+        {
+            var row = document.createElement("tr");
+            
+            //id
+            var cell1 = document.createElement("td");
+            var cell1t = document.createTextNode(favoriteList[i].id);
+           
+          
+            //url
+            var cell2 = document.createElement("a");
+            var cell2t = document.createTextNode(favoriteList[i].url);
+            var myLink = favoriteList[i].url;
+            cell2.href =myLink;
+            
+            //description
+            var cell3 = document.createElement("td");
+            var cell3t = document.createTextNode(favoriteList[i].description);
+            
+            //favorite buttton
+            var cell4 = document.createElement("button");          
+            cell4.type="button";
+            cell4.innerHTML = " Remove";
+            cell4.setAttribute("gistId", favoriteList[i].id);
+            cell4.onclick = function (){
+                var gistId = this.getAttribute("gistId");
+                removeFavList (gistId);
+            }
+          
+          
+         
+            
+            
+            //add text to cells
+            cell1.appendChild(cell1t);
+            cell2.appendChild(cell2t);
+            cell3.appendChild(cell3t);
+          
+            
+            
+            
+            
+            //add cells to row
+            row.appendChild(cell1);
+            row.appendChild(cell2);
+            row.appendChild(cell3);
+            row.appendChild(cell4);
+            tblBody.appendChild(row);
+            
+        }
+        
+        //add rows to main table
+       
+        tbl.appendChild(tblBody);
+        tableTop.appendChild(tbl); 
+        
+        
+        
+        
+    }
+    
+   function removeFavList (gistId)
+    {
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
